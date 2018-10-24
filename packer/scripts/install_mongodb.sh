17e9b13255f84451afed712d887e70ba3a412eb4
Max Mayorovskiy <max.mayorovsky@gmail.com>
2018-10-24 21:53:51 +0300

    Moved scripts and fixed path to the scrips

--- packer/scripts/install_mongodb.sh
+++ packer/scripts/install_mongodb.sh
@@ -5,4 +5,4 @@ bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 mul
 apt update
 apt install -y mongodb-org
 systemctl start mongod
-systemctl enable mongod
+systemctl enable mongodpwd
